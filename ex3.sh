f4121429c8183e0fd49f541827549439829b2f59
Afanasii05 <fanelmunteanu568@gmail.com>
2024-11-18 17:07:54 +0000

    modificat

--- ex3.sh
+++ ex3.sh
@@ -3,11 +3,11 @@
 cat /etc/hosts | while read ip dom
 do
 	echo "ip: $ip, dom: $dom"
-	if [ $ip = "#" ]
+	if [ $ip = "##" ]
 	then
 		break
 	fi
-	output=$(nslookup "$dom" 8.8.8.8 | grep -m 2 '^Address:' | awk '{print $2}' | tail -n 1 )
+	output=$(nslookup "$dom" 3.3.3.4 | grep -m 2 '^Address:' | awk '{print $1}' | tail -n 1 )
 
 	if [[ "$output" != "$ip" ]]; then
 	  echo "Bonus Ip for $dom in/etc/hosts" 
